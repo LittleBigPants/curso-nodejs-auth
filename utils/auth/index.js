fef5a4f5d3010cbf2eb5b3b0ed5c7e3bf4f2d057
LittleBigPants <juanmanuel_garcia98@hotmail.com>
2023-12-28 13:16:40 -0300

    jwt-strategy

--- utils/auth/index.js
+++ utils/auth/index.js
@@ -1,5 +1,7 @@
 const passport = require('passport');
 const LocalStrategy = require('./strategies/local.strategy');
+const jwtEstrategy = require('./strategies/jwt.strategy');
 
 
 passport.use(LocalStrategy);
+passport.use(jwtEstrategy);
